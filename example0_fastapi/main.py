de0c21f241a040931f46e08e97391e654deb45b1
pdiosquez467 <pablodiosquez467@gmail.com>
2025-09-30 16:24:57 -0300

    Agrego una clase 'Alumno' y un endpoint para testear

--- example0_fastapi/main.py
+++ example0_fastapi/main.py
@@ -1,8 +1,30 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Alumno(BaseModel):
+    padron: int
+    nombre: str
+    apelido: str
+    edad: int | None = None
+
+
+alumnos: list[Alumno] = [
+    Alumno(padron=1015, nombre="Walter", apelido="White", edad=52),
+    Alumno(padron=1022, nombre="Jesse", apelido="Pinkman", edad=26),
+    Alumno(padron=1039, nombre="Skyler", apelido="White", edad=45),
+    Alumno(padron=1047, nombre="Saul", apelido="Goodman", edad=49),
+    Alumno(padron=1054, nombre="Gustavo", apelido="Fring", edad=50),
+]
 
 
 @app.get("/")
 def root():
     return {"message": "Hello, World!"}
+
+
+@app.get("/alumnos")
+def list() -> list[Alumno]:
+    return alumnos
